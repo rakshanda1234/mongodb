fc6c00c2f4e3d2a21e43d77f33be17314603167c
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2023-03-27 16:27:34 +0530

    task12- user can see order he has placed

--- models/user.js
+++ models/user.js
@@ -56,10 +56,15 @@ userSchema.methods.removeFromCart = function (productId) {
   return this.save();
 };
 
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 
-// const mongodb = require("mongodb");
-// const getDb = require("../util/database").getDb;
+// const mongodb = require('mongodb');
+// const getDb = require('../util/database').getDb;
 
 // const ObjectId = mongodb.ObjectId;
 
@@ -73,11 +78,11 @@ module.exports = mongoose.model("User", userSchema);
 
 //   save() {
 //     const db = getDb();
-//     return db.collection("users").insertOne(this);
+//     return db.collection('users').insertOne(this);
 //   }
 
 //   addToCart(product) {
-//     const cartProductIndex = this.cart.items.findIndex((cp) => {
+//     const cartProductIndex = this.cart.items.findIndex(cp => {
 //       return cp.productId.toString() === product._id.toString();
 //     });
 //     let newQuantity = 1;
@@ -89,15 +94,15 @@ module.exports = mongoose.model("User", userSchema);
 //     } else {
 //       updatedCartItems.push({
 //         productId: new ObjectId(product._id),
-//         quantity: newQuantity,
+//         quantity: newQuantity
 //       });
 //     }
 //     const updatedCart = {
-//       items: updatedCartItems,
+//       items: updatedCartItems
 //     };
 //     const db = getDb();
 //     return db
-//       .collection("users")
+//       .collection('users')
 //       .updateOne(
 //         { _id: new ObjectId(this._id) },
 //         { $set: { cart: updatedCart } }
@@ -106,32 +111,32 @@ module.exports = mongoose.model("User", userSchema);
 
 //   getCart() {
 //     const db = getDb();
-//     const productIds = this.cart.items.map((i) => {
+//     const productIds = this.cart.items.map(i => {
 //       return i.productId;
 //     });
 //     return db
-//       .collection("products")
+//       .collection('products')
 //       .find({ _id: { $in: productIds } })
 //       .toArray()
-//       .then((products) => {
-//         return products.map((p) => {
+//       .then(products => {
+//         return products.map(p => {
 //           return {
 //             ...p,
-//             quantity: this.cart.items.find((i) => {
+//             quantity: this.cart.items.find(i => {
 //               return i.productId.toString() === p._id.toString();
-//             }).quantity,
+//             }).quantity
 //           };
 //         });
 //       });
 //   }
 
 //   deleteItemFromCart(productId) {
-//     const updatedCartItems = this.cart.items.filter((item) => {
+//     const updatedCartItems = this.cart.items.filter(item => {
 //       return item.productId.toString() !== productId.toString();
 //     });
 //     const db = getDb();
 //     return db
-//       .collection("users")
+//       .collection('users')
 //       .updateOne(
 //         { _id: new ObjectId(this._id) },
 //         { $set: { cart: { items: updatedCartItems } } }
@@ -141,20 +146,20 @@ module.exports = mongoose.model("User", userSchema);
 //   addOrder() {
 //     const db = getDb();
 //     return this.getCart()
-//       .then((products) => {
+//       .then(products => {
 //         const order = {
 //           items: products,
 //           user: {
 //             _id: new ObjectId(this._id),
-//             name: this.name,
-//           },
+//             name: this.name
+//           }
 //         };
-//         return db.collection("orders").insertOne(order);
+//         return db.collection('orders').insertOne(order);
 //       })
-//       .then((result) => {
+//       .then(result => {
 //         this.cart = { items: [] };
 //         return db
-//           .collection("users")
+//           .collection('users')
 //           .updateOne(
 //             { _id: new ObjectId(this._id) },
 //             { $set: { cart: { items: [] } } }
@@ -165,21 +170,21 @@ module.exports = mongoose.model("User", userSchema);
 //   getOrders() {
 //     const db = getDb();
 //     return db
-//       .collection("orders")
-//       .find({ "user._id": new ObjectId(this._id) })
+//       .collection('orders')
+//       .find({ 'user._id': new ObjectId(this._id) })
 //       .toArray();
 //   }
 
 //   static findById(userId) {
 //     const db = getDb();
 //     return db
-//       .collection("users")
+//       .collection('users')
 //       .findOne({ _id: new ObjectId(userId) })
-//       .then((user) => {
+//       .then(user => {
 //         console.log(user);
 //         return user;
 //       })
-//       .catch((err) => {
+//       .catch(err => {
 //         console.log(err);
 //       });
 //   }
